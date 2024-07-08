90d68de7bf47214a83e068081b8fe87a405cea22
Sergei Malinin <s@malinin.dev>
2024-07-08 21:16:49 +0700

    add prettier

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,3 @@
 export const App = () => {
-  return (
-    <div>
-      hello
-    </div>
-  )
-}
+  return <div>hello</div>;
+};
--- src/App/index.ts
+++ src/App/index.ts
@@ -1,2 +1,1 @@
-export { App } from "./App";
-
+export { App } from './App';
